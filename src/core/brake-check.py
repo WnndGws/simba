4b3d9dbb6f175e647e82764c5263f77b9f38cf5b
Wynand Gouws <WnndGws@users.noreply.github.com>
2026-05-08 12:27:49 +1000

    20260508_122749

--- src/core/brake-check.py
+++ src/core/brake-check.py
@@ -5,7 +5,7 @@ from multiprocessing import Process, Queue, shared_memory
 from loguru import logger
 from rich.logging import RichHandler
 
-from utils import udp_processor, udp_receiver, udp_simulator
+from utils import brake_point_finder, udp_processor, udp_receiver, udp_simulator
 
 #
 # Setup logger with RichHandler for better output
@@ -17,11 +17,5 @@ logger.add(
 logger.add("udp.log", level="CRITICAL", format="{message}")
 
 if __name__ == "__main__":
-    # receiver = Process(target=udp_receiver.udp_receiver, daemon=True)
-    receiver = Process(target=udp_simulator.udp_receiver, daemon=True)
-    receiver.start()
-
-    queue = Queue()
-    udp_processor.process_named_shared_memory(
-        output_queue=queue, decode_list=["lapdata"]
-    )
+    q = brake_point_finder.create_queue()
+    brake_point_finder.yield_queue(q)
